86a941c836fcbce481433056986ee552aa7c5da6
11-CarynMah <91871034+11-CarynMah@users.noreply.github.com>
2021-10-22 03:18:50 +0800

    Edited TestSuite
    Added test
    subtraction
    multiplication
    division

--- Lab10 - Project Starter/Assets/Tests/TestSuite.cs
+++ Lab10 - Project Starter/Assets/Tests/TestSuite.cs
@@ -31,5 +31,68 @@ namespace Tests
             result = Calculator.CalculatePair(5, 2, "+");
             Assert.AreEqual(result, 7);
         }
+
+        [Test]
+        public void TestSubtraction()
+        {
+            // Use the Assert class to test conditions
+            result = Calculator.CalculatePair(5, 2, "-");
+            Assert.AreEqual(result, 3);
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator TestUnitySubtraction()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+
+            result = Calculator.CalculatePair(5, 2, "-");
+            Assert.AreEqual(result, 3);
+        }
+
+        [Test]
+        public void TestMultiplication()
+        {
+            // Use the Assert class to test conditions
+            result = Calculator.CalculatePair(5, 2, "*");
+            Assert.AreEqual(result, 10);
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator TestUnityMultiplication()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+
+            result = Calculator.CalculatePair(5, 2, "*");
+            Assert.AreEqual(result, 10);
+        }
+
+        [Test]
+        public void TestDivision()
+        {
+            // Use the Assert class to test conditions
+            result = Calculator.CalculatePair(5, 2, "/");
+            Assert.AreEqual(result, 2.5f);
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator TestUnityDivision()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+
+            result = Calculator.CalculatePair(5, 2, "/");
+            Assert.AreEqual(result, 2.5f);
+        }
     }
 }
